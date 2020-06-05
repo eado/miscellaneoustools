a624e2e81eddc0172e978fcb97fb838a17ebc7ee
Omar Elamri <ea_do@icloud.com>
2020-06-05 12:38:16 -0500

    add editsnipe

--- main.ts
+++ main.ts
@@ -6,6 +6,7 @@ const token = JSON.parse(readFileSync("config.json", 'utf8')).token
 const client = new Client();
 
 const messageCache: {[id: string]: Message[]} = {}
+const messageEditCache: {[id: string]: Message[]} = {}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -15,11 +16,23 @@ client.on('messageDelete', (message) => {
     if (!message.guild) {
         return
     }
-    console.log(message)
+    
     if (messageCache[message.channel.id]) {
         messageCache[message.channel.id].push(message)
     } else {
         messageCache[message.channel.id] = [message]
+    }
+})
+
+client.on('messageUpdate', (old, _) => {
+    if (!old.guild) {
+        return
+    }
+    
+    if (messageEditCache[old.channel.id]) {
+        messageEditCache[old.channel.id].push(old)
+    } else {
+        messageEditCache[old.channel.id] = [old]
     }
 })
 
@@ -44,6 +57,19 @@ client.on('message', (message) => {
             }
 
             message.channel.send(`${deletedMessage.author}: ${deletedMessage.content}`)
+        } else if (method == "editsnipe") {
+            if (!messageEditCache[message.channel.id]) {
+                message.channel.send("There are no recently edited messages!")
+                return
+            }
+            
+            const editedMessage = messageEditCache[message.channel.id].pop()
+            if (!editedMessage) {
+                message.channel.send("There are no recently edited messages!")
+                return
+            }
+
+            message.channel.send(`${editedMessage.author}: ${editedMessage.content}`)
         } else if (method == "help") {
             message.channel.send("Welcome to the Miscellaneous Tools Bot! It currently doesn't do much, but feel free to suggest functionality. \nFunctions:\n- Snipe: Retrieve previously deleted message.")
         }
