e89032c9bfb4d45d95271a8033b13c5113e3c59f
Omar Elamri <ea_do@icloud.com>
2021-03-25 16:41:03 -0500

    fix bugs

--- main.ts
+++ main.ts
@@ -6,6 +6,9 @@ import {
   TextChannel,
   User,
   MessageReaction,
+  PartialMessage,
+  PartialUser,
+  GuildMemberRoleManager,
 } from "discord.js";
 import { readFileSync } from "fs";
 import moment from "moment";
@@ -14,10 +17,10 @@ const token = JSON.parse(readFileSync("config.json", "utf8")).token;
 
 const client = new Client();
 
-const messageCache: { [id: string]: Message[] } = {};
-const messageEditCache: { [id: string]: Message[] } = {};
+const messageCache: { [id: string]: (Message | PartialMessage)[] } = {};
+const messageEditCache: { [id: string]: (Message | PartialMessage)[] } = {};
 const reactionCache: {
-  [id: string]: { reaction: MessageReaction; user: User }[];
+  [id: string]: { reaction: MessageReaction; user: User | PartialUser }[];
 } = {};
 const rolesCache: {
   [id: string]: { roles: Collection<string, Role>; isSelf: boolean };
@@ -35,9 +38,12 @@ const cleanFormatting = (str: string) => {
 const jemoticonstring = readFileSync("jemoticons.txt", "utf8")
 const jemoticons = jemoticonstring.split("\n").filter(j => j !== "").map(cleanFormatting)
 
-client.on("ready", () => {
+client.on("ready", async () => {
+  if (!client.user) {
+    return
+  }
   console.log(`Logged in as ${client.user.tag}`);
-
+  
   client.user.setActivity("Type //help for help");
 
 });
@@ -78,11 +84,14 @@ client.on("messageReactionRemove", (reaction, user) => {
   }
 });
 
-client.on("message", (message) => {
+client.on("message", async (message) => {
   if (!message.guild) {
     return;
   }
 
+  if (message.author.bot) {
+    return
+  }
 
   if (message.content.startsWith("//")) {
     const method = message.content.split(" ")[0].replace("//", "");
@@ -106,8 +115,7 @@ client.on("message", (message) => {
 
       let messageString = "";
       if (
-        deletedMessage.content.startsWith("<@") &&
-        deletedMessage.content.startsWith("> @", 20)
+        deletedMessage.content?.match(".* on.*\n>")
       ) {
         messageString = deletedMessage.content;
       } else {
@@ -115,7 +123,7 @@ client.on("message", (message) => {
           "MMMM Do, h:mm:ss a"
         );
 
-        messageString = `${deletedMessage.author} on ${messageTime}\n> ${deletedMessage.content}`;
+        messageString = `${deletedMessage.author?.toString()} on ${messageTime}\n> ${deletedMessage.content}`;
       }
 
       for (let attach of Array.from(deletedMessage.attachments.values())) {
@@ -138,13 +146,13 @@ client.on("message", (message) => {
       let messageTime = moment(editedMessage.createdTimestamp).format(
         "MMMM Do, h:mm:ss a"
       );
-      let messageString = `${editedMessage.author} on ${messageTime}\n> ${editedMessage.content}`;
+      let messageString = `${editedMessage.author?.toString()} on ${messageTime}\n> ${editedMessage.content}`;
 
       for (let attach of Array.from(editedMessage.attachments.values())) {
         messageString += attach.proxyURL + " ";
       }
 
-      message.channel.send(messageString);
+      message.channel.send(messageString)
     } else if (method == "reactionsnipe" || method == "r") {
       if (!reactionCache[message.channel.id]) {
         message.channel.send("There are no recently removed reactions!");
@@ -157,105 +165,110 @@ client.on("message", (message) => {
         return;
       }
 
-      let messageString = `${reaction.user} reacted to "${reaction.reaction.message.cleanContent}" with ${reaction.reaction.emoji}`;
+      let messageString = `${reaction.user.toString()} reacted to "${reaction.reaction.message.cleanContent}" with ${reaction.reaction.emoji.toString()}`;
 
       message.channel.send(messageString);
-    } else if (method == "gulag" || method == "g") {
-      const user = message.author;
-      const member = message.guild.member(user);
+    // } else if (method == "gulag" || method == "g") {
+    //   const user = message.author;
+    //   const member = message.guild.member(user);
 
-      if (user.id === "638089451054039040") {
-        message.channel.send("no");
-        return;
-      }
+    //   if (user.id === "638089451054039040") {
+    //     message.channel.send("no");
+    //     return;
+    //   }
 
-      const gulaged = message.mentions.users.first();
-      if (!gulaged) {
-        message.channel.send("Make sure to mention a user to gulag.");
-        return;
-      }
-      if (member.hasPermission("MANAGE_GUILD")) {
-        const gulagedMember = message.guild.member(gulaged);
-        if (
-          (gulagedMember.highestRole.comparePositionTo(member.highestRole) >
-            -1 &&
-          gulaged.id != user.id) && (user.id != "664137899347935253")
-        ) {
-          message.channel.send(
-            `${gulaged.username} is either the same or higher than you in the role hierarchy :(`
-          );
-          return;
-        }
-        if (rolesCache[gulagedMember.id]) {
-          message.channel.send(`${gulaged.username} is already in the gulag :(`)
-          return
-        }
-        const roles = gulagedMember.roles;
-        rolesCache[gulagedMember.id] = {
-          roles,
-          isSelf: user.id === gulaged.id,
-        };
-        gulagedMember.setRoles(
-          message.guild.roles.filter((role) => role.name === "UltraGulag")
-        );
-        message.channel.send(`${gulaged.username} has been gulaged :(`);
+    //   const gulaged = message.mentions.users.first();
+    //   if (!gulaged) {
+    //     message.channel.send("Make sure to mention a user to gulag.");
+    //     return;
+    //   }
+    //   if (member?.hasPermission("MANAGE_GUILD")) {
+    //     const gulagedMember = message.guild.member(gulaged);
+    //     if (!gulagedMember) {
+    //       message.channel.send("User doesn't exist :(")
+    //       return
+    //     }
+    //     if (
+    //       (gulagedMember?.roles?.highest?.position ? gulagedMember.roles.highest.position : 0  >= member.roles.highest.position
+    //        &&
+    //       gulaged.id != user.id) && (user.id != "664137899347935253")
+    //     ) {
+    //       message.channel.send(
+    //         `${gulaged.username} is either the same or higher than you in the role hierarchy :(`
+    //       );
+    //       return;
+    //     }
+    //     if (gulagedMember && rolesCache[gulagedMember?.id]) {
+    //       message.channel.send(`${gulaged.username} is already in the gulag :(`)
+    //       return
+    //     }
+    //     const roles = gulagedMember?.roles;
+    //     rolesCache[gulagedMember.id] = {
+    //       roles: roles.cache,
+    //       isSelf: user.id === gulaged.id,
+    //     };
+    //     gulagedMember.roles.set(await message.guild.roles.fetch("UltraGulag"))
+    //     gulagedMember.setRoles(
+    //       message.guild.roles.filter((role) => role.name === "UltraGulag")
+    //     );
+    //     message.channel.send(`${gulaged.username} has been gulaged :(`);
         
-      } else {
-        message.channel.send("You do not have permission to run that command!");
-      }
-    } else if (method == "ungulag" || method == "u") {
-      const user = message.author;
-      const member = message.guild.member(user);
+    //   } else {
+    //     message.channel.send("You do not have permission to run that command!");
+    //   }
+    // } else if (method == "ungulag" || method == "u") {
+    //   const user = message.author;
+    //   const member = message.guild.member(user);
 
-      if (user.id === "638089451054039040") {
-        message.channel.send("no");
-        return;
-      }
+    //   if (user.id === "638089451054039040") {
+    //     message.channel.send("no");
+    //     return;
+    //   }
 
-      const ungulaged = message.mentions.users.first();
-      if (!ungulaged) {
-        message.channel.send("Make sure to mention a user to ungulag.");
-        return;
-      }
-      if (member.hasPermission("MANAGE_GUILD")) {
-        const ungulagedMember = message.guild.member(ungulaged);
-        if (
-          (ungulagedMember.highestRole.comparePositionTo(member.highestRole) >
-            -1 &&
-          ungulaged.id != user.id) && (user.id != "664137899347935253")
-        ) {
-          message.channel.send(
-            `${ungulaged.username} is either the same or higher than you in the role hierarchy :(`
-          );
-          return;
-        }
+    //   const ungulaged = message.mentions.users.first();
+    //   if (!ungulaged) {
+    //     message.channel.send("Make sure to mention a user to ungulag.");
+    //     return;
+    //   }
+    //   if (member.hasPermission("MANAGE_GUILD")) {
+    //     const ungulagedMember = message.guild.member(ungulaged);
+    //     if (
+    //       (ungulagedMember.highestRole.comparePositionTo(member.highestRole) >
+    //         -1 &&
+    //       ungulaged.id != user.id) && (user.id != "664137899347935253")
+    //     ) {
+    //       message.channel.send(
+    //         `${ungulaged.username} is either the same or higher than you in the role hierarchy :(`
+    //       );
+    //       return;
+    //     }
 
-        const roles = rolesCache[ungulaged.id];
+    //     const roles = rolesCache[ungulaged.id];
 
-        if (ungulaged.id === user.id && !roles.isSelf) {
-          message.channel.send(
-            "Someone else put you in the gulag. Please wait until they (or someone else with appropriate permissions) takes you out."
-          );
-          return;
-        }
+    //     if (ungulaged.id === user.id && !roles.isSelf) {
+    //       message.channel.send(
+    //         "Someone else put you in the gulag. Please wait until they (or someone else with appropriate permissions) takes you out."
+    //       );
+    //       return;
+    //     }
 
-        if (!roles) {
-          message.channel.send("This user isn't currently in the gulag :)");
-          return;
-        }
-        ungulagedMember.setRoles(roles.roles);
-        message.channel.send(
-          `${ungulaged.username} has been taken out of the gulag :)`
-        );
-        delete rolesCache[ungulaged.id]
-      } else {
-        message.channel.send("You do not have permission to run that command!");
-      }
+    //     if (!roles) {
+    //       message.channel.send("This user isn't currently in the gulag :)");
+    //       return;
+    //     }
+    //     ungulagedMember?.roles.set(roles.roles.);
+    //     message.channel.send(
+    //       `${ungulaged.username} has been taken out of the gulag :)`
+    //     );
+    //     delete rolesCache[ungulaged.id]
+    //   } else {
+    //     message.channel.send("You do not have permission to run that command!");
+    //   }
     } else if (method == "mock" || method == "m") {
-      const mock = (m: Message) => {
+      const mock = (m: Message | undefined) => {
         message.channel.send(
           "> " +
-            m.content
+            m?.content
               .split("")
               .map((v, i) => (i % 2 == 0 ? v.toLowerCase() : v.toUpperCase()))
               .join("")
@@ -276,15 +289,15 @@ client.on("message", (message) => {
         if (urlscheme.length > 6) {
           const chID = urlscheme[5];
           const messageID = urlscheme[6];
-          const channel = client.channels.find(
+          const channel = client.channels.cache.find(
             (ch) => chID === ch.id
           ) as TextChannel;
           if (!channel) {
             sendFail();
             return;
           }
-          channel
-            .fetchMessage(messageID)
+          (await channel.fetch() as TextChannel)
+            .messages.fetch(messageID)
             .then((m) => {
               if (!m) {
                 sendFail();
@@ -301,8 +314,8 @@ client.on("message", (message) => {
           sendFail();
         }
       } else {
-        message.channel.fetchMessages({ limit: 2 }).then((messages) => {
-          mock(messages.last());
+        (message.channel as TextChannel).messages.fetch({ limit: 2 }).then((messages) => {
+          mock(messages.last())
           message.delete();
         });
       }
@@ -310,7 +323,7 @@ client.on("message", (message) => {
         message.channel.send(jemoticons[Math.floor(Math.random() * jemoticons.length)])
     } else if (method == "help" || method == "h") {
       message.channel.send(
-        "Welcome to the Miscellaneous Tools Bot! It currently doesn't do much, but feel free to suggest functionality. \nFunctions:\n- (s)nipe: Retrieve previously deleted messages\n- (e)ditsnipe: Retrieve previous revisions of messages\n- (g)ulag: Remove all roles for a user and add an extra role for punishment\n- (u)ngulag: Restore all roles for a user after using (g)ulag\n- (m)ock: Changes capitalization of annoying messages\n- (r)eactionsnipe: Retrieve removed reaction\n- (j)emoticon: Print a random jemoticon\n- (h)elp: Show this help message"
+        "Welcome to the Miscellaneous Tools Bot! It currently doesn't do much, but feel free to suggest functionality. \nFunctions:\n- (s)nipe: Retrieve previously deleted messages\n- (e)ditsnipe: Retrieve previous revisions of messages \n- (m)ock: Changes capitalization of annoying messages\n- (r)eactionsnipe: Retrieve removed reaction\n- (j)emoticon: Print a random jemoticon\n- (h)elp: Show this help message"
       );
     }
   }
